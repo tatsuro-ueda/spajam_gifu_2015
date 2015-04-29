03772a978a001395bec45c8b9f26f42aefae5eec
thirauti <thirauti@gmail.com>
2015-04-29 21:34:51 +0900

    Base64で音声ファイルを受け取って解析するメソッド追加　※クライアントがないため未テスト

--- SpajamMadobenWebAPI/SpajamMadobenWebAPI/Controllers/ServiceController.cs
+++ SpajamMadobenWebAPI/SpajamMadobenWebAPI/Controllers/ServiceController.cs
@@ -46,6 +46,48 @@ namespace SpajamMadobenWebAPI.Controllers
             return null;
         }
 
+        // GET: api/GoogleSpeechTexts/base64
+        public string GetFlacSpeechText(String base64)
+        {
+            var key = "AIzaSyBlwhF2pGCf472kxOMCGk1-4ODWtInjjGk";
+            var url = "https://www.google.com/speech-api/v2/recognize?output=json&lang=ja-jp&key=";
+            var postUrl = url + key;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(postUrl);
+
+            /*
+            //ファイルに保存する
+            //保存するファイル名
+            string outFileName = @"C:\test.dat";
+
+            //ファイルに書き込む
+            System.IO.FileStream outFile = new System.IO.FileStream(outFileName,
+                System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            outFile.Write(byteArray, 0, bs.Length);
+            outFile.Close();
+            */
+
+            // var filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + flacName + ".flac");
+            // byte[] byteArray = File.ReadAllBytes(filePath);
+
+            request.Method = "POST";
+            //バイト型配列に戻す
+            byte[] byteArray = System.Convert.FromBase64String(base64);
+            var sampleRate = 16000;
+            request.ContentType = "audio/x-flac; rate=" + sampleRate.ToString();
+            request.ContentLength = byteArray.Length;
+
+            Stream sendStream = request.GetRequestStream();
+            sendStream.Write(byteArray, 0, byteArray.Length);
+            sendStream.Close();
+
+            string responseFromServer;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var reader = new StreamReader(response.GetResponseStream());
+            responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            return responseFromServer;
+        }
 
         // GET: api/GoogleSpeechTexts/flacName
         public string GetGoogleSpeechText(String flacName)
