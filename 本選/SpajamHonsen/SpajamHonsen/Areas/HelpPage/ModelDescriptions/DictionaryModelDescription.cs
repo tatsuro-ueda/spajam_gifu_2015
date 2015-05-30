7a841e34089d17ce2423cf9be748723397085c1f
thirauti <thirauti@gmail.com>
2015-05-30 10:58:58 +0900

    本選用のWEBAPIプロジェクトを作成

---
+++ 本選/SpajamHonsen/SpajamHonsen/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SpajamHonsen.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ 本選/SpajamHonsen/SpajamHonsen/Controllers/TalksController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SpajamHonsen.Models;
+
+namespace SpajamHonsen.Controllers
+{
+    public class TalksController : ApiController
+    {
+        private SpajamHonsenDBEntities db = new SpajamHonsenDBEntities();
+
+        // GET: api/Talks
+        public IQueryable<Talk> GetTalk()
+        {
+            return db.Talk;
+        }
+
+        // GET: api/Talks/5
+        [ResponseType(typeof(Talk))]
+        public async Task<IHttpActionResult> GetTalk(string id)
+        {
+            Talk talk = await db.Talk.FindAsync(id);
+            if (talk == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(talk);
+        }
+
+        // PUT: api/Talks/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutTalk(string id, Talk talk)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != talk.UserID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(talk).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TalkExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Talks
+        [ResponseType(typeof(Talk))]
+        public async Task<IHttpActionResult> PostTalk(Talk talk)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Talk.Add(talk);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TalkExists(talk.UserID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = talk.UserID }, talk);
+        }
+
+        // DELETE: api/Talks/5
+        [ResponseType(typeof(Talk))]
+        public async Task<IHttpActionResult> DeleteTalk(string id)
+        {
+            Talk talk = await db.Talk.FindAsync(id);
+            if (talk == null)
+            {
+                return NotFound();
+            }
+
+            db.Talk.Remove(talk);
+            await db.SaveChangesAsync();
+
+            return Ok(talk);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TalkExists(string id)
+        {
+            return db.Talk.Count(e => e.UserID == id) > 0;
+        }
+    }
+}
