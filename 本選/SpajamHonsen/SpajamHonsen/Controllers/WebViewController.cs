d9ef89266062b3182156d19c00208785b984a811
thirauti <thirauti@gmail.com>
2015-05-30 22:08:31 +0900

    WebView用のViewを追加

---
+++ 本選/SpajamHonsen/SpajamHonsen/Controllers/WebViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpajamHonsen.Controllers
+{
+    public class WebViewController : Controller
+    {
+        // GET: WebView
+        public ActionResult Index()
+        {
+            return View();
+        }   
+    }
+}
