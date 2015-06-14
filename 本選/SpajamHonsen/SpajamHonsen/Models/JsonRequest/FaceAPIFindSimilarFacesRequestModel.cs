099760853c9390a3aef290c7d2ee82b9e34fc668
thirauti <thirauti@gmail.com>
2015-06-14 13:26:37 +0900

    FaceAPIFindSimilarFacesの検証完了

---
+++ 本選/SpajamHonsen/SpajamHonsen/Models/JsonRequest/FaceAPIFindSimilarFacesRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpajamHonsen.Models.JsonRequest
+{
+    /// <summary>
+    /// FaceAPIFindSimilarFacesのRequestModel
+    /// </summary>
+    public class FaceAPIFindSimilarFacesRequestModel
+    {
+        public string faceId { get; set; }
+        public string[] faceIds { get; set; }
+    }
+}
