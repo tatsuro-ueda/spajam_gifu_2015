90e85ab5017ce3045659211261590a173d659f0d
thirauti <thirauti@gmail.com>
2015-06-14 14:35:07 +0900

    FaceAPIIdentificationの検証完了

---
+++ 本選/SpajamHonsen/SpajamHonsen/Models/JsonRequest/FaceAPIIdentificationRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpajamHonsen.Models.JsonRequest
+{
+    /// <summary>
+    /// FaceAPIIdentificationのRequestModel
+    /// </summary>
+    public class FaceAPIIdentificationRequestModel
+    {
+        public string[] faceIds { get; set; }
+        public string personGroupId { get; set; }
+        public int maxNumOfCandidatesReturned { get; set; }
+    }
+}
---
+++ 本選/SpajamHonsen/SpajamHonsen/Models/JsonResponse/FaceAPIIdentificationResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpajamHonsen.Models.JsonResponse
+{
+    /// <summary>
+    /// FaceAPIIdentificationのResponseModel
+    /// </summary>
+    public class FaceAPIIdentificationResponseModel
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public string faceId { get; set; }
+        public Candidate[] candidates { get; set; }
+    }
+
+    public class Candidate
+    {
+        public string personId { get; set; }
+        public string name { get; set; }
+        public float confidence { get; set; }
+    }
+}
